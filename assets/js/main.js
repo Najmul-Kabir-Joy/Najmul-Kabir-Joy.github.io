5afa3bc72c2a0493894a6e9efe14b5a372b7ebfd
Najmul-Kabir-Joy <najmulkabir178@gmail.com>
2021-11-29 23:47:05 +0600

    new updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,41 +4,41 @@ const navMenu = document.getElementById('nav-menu'),
     closeMenu = document.getElementById('nav-close')
 
 // SHOW
-toggleMenu.addEventListener('click',()=>{
+toggleMenu.addEventListener('click', () => {
     navMenu.classList.toggle('show')
 })
 
 // HIDDEN
-closeMenu.addEventListener('click',()=>{
+closeMenu.addEventListener('click', () => {
     navMenu.classList.remove('show')
 })
 
 /*===== ACTIVE AND REMOVE MENU =====*/
 const navlink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     navMenu.classList.remove('show')
 }
 
-navlink.forEach(n => n.addEventListener('click',linkAction))
+navlink.forEach(n => n.addEventListener('click', linkAction))
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
 const sections = document.querySelectorAll('section[id]')
 
 window.addEventListener('scroll', scrollActive)
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY<= sectionTop + sectionHeight){
-            document.querySelectorAll('.nav__menu a[href*='+ sectionId + ']').forEach.classList.add('active')
-        }else
-        document.querySelectorAll('.nav__menu a[href*='+ sectionId + ']').forEach.classList.remove('active')
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelectorAll('.nav__menu a[href*=' + sectionId + ']').forEach.classList.add('active')
+        } else
+            document.querySelectorAll('.nav__menu a[href*=' + sectionId + ']').forEach.classList.remove('active')
     })
 }
 
@@ -55,14 +55,25 @@ function sendmail() {
     let subject = $("#subject").val();
     let body = $("#message").val();
     let name = $("#name").val();
-  
+
     console.log(name, body, subject);
-  
+
     body = `${body}<br>Regards<br>` + name;
-  
+
     setTimeout(
-      window.open(`mailto:najmulkabir178@gmail.com?subject=${subject}&body=${body}`),
-      100000
+        window.open(`mailto:najmulkabir178@gmail.com?subject=${subject}&body=${body}`),
+        100000
     );
-  }
+}
+
+const btn = document.querySelector('.resume__button');
+btn.onmousemove = function (e) {
+    const x = e.pageX - btn.offsetLeft;
+    const y = e.pageY - btn.offsetTop;
+
+    btn.style.setProperty('--x', x + 'px');
+    btn.style.setProperty('--y', y + 'px');
+};
+
+
 
